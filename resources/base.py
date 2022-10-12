f79522beb4a31a6204a9760b084e2e4dba1bcf4d
mbay <murat.bayram@softwareag.com>
2022-10-12 10:54:38 +0200

    adding cache

--- resources/base.py
+++ resources/base.py
@@ -4,13 +4,14 @@ from utils.rest import log
 from flask import jsonify, make_response, request, abort, Flask
 
 from datetime import datetime
+from cacheout import Cache
 
-import time
 import logging
+import time
 from functools import wraps
 from API.inventory import get_internalId_from_externalId
 
-
+cache = Cache(maxsize=1024, ttl=0, timer=time.time, default=None)
 class BaseRequest(Resource):
     logger = logging.getLogger('__main__.' + __name__)
     @log
@@ -33,8 +34,19 @@ class BaseRequest(Resource):
             if not self.id or not self.type:
                 self.logger.debug("Either id or type are empty, returning 500.")
                 return make_response(jsonify({"message": str("externalId and/or type is missing as parameter")}),500)
-            self.source = get_internalId_from_externalId(self.type,self.id)
-            self.logger.debug(type(self.source))
+            # Creating a json string that contains the id and the type such that if the interal id was already queried there is no need to requery it.
+            self.logger.debug("Creating the json string to validate with cache.")
+            self.cache_key = str({"id": self.id, "type": self.type})
+            self.logger.debug(f'The json string for cache looks: {self.cache_key}.')
+            if cache.get(self.cache_key) not None:
+                self.logger.debug("Cache does contain the internal id already.")
+                self.source = cache.get(self.cache_key)
+                self.logger.debug(f'The internal id from cache is: {self.source}')
+            else:
+                self.logger.debug("Cache does not contain the internal id, query it from identity API.")
+                self.source = get_internalId_from_externalId(self.type,self.id)
+                cache.set(self.cache_key,self.source)
+                self.logger.debug(f'The following internal id was written to cache: {self.source}')
             try:
                 self.logger.debug(f'Checking if source is an response object or an real externalId.')
                 self.status_code = self.source.status_code
